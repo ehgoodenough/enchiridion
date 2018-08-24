4482f2d569083686d83af63b4cb28d4b2d07dfc9
Andrew McPherson <ehgoodenough@gmail.com>
2018-08-23 22:06:14 -0700

    Copied in some old monster code, for reference.

--- client/source/models/Monster.js
+++ client/source/models/Monster.js
@@ -39,3 +39,81 @@ export default class Monster {
         // ...
     }
 }
+
+// class Monster extends Entity {
+//     constructor() {
+//         // ...
+//         if(!protomonster.position) {
+//             if(Math.random() < 0.5) {
+//                 protomonster.position = {
+//                     x: Math.floor(Math.random() * SIZE),
+//                     y: Math.random() < 0.5 ? -1 : SIZE
+//                 }
+//             } else {
+//                 protomonster.position = {
+//                     x: Math.random() < 0.5 ? -1 : SIZE,
+//                     y: Math.floor(Math.random() * SIZE)
+//                 }
+//             }
+//         }
+//         this.isPrepared = Math.random() < 0.5
+//     }
+//     op() {
+//         if(this.isPrepared == false) {
+//             this.isPrepared = true
+//             this.symbol = "G"
+//         } else if(this.isPrepared = true) {
+//             this.isPrepared = false
+//             this.symbol = "g"
+//
+//             // move towards the hero, prioritzing
+//             // whichever vector has a longer magnitude.
+//             if(Math.abs(this.position.y - ((hero.position.y + hero.prevposition.y) / 2))
+//             >= Math.abs(this.position.x - ((hero.position.x + hero.prevposition.x) / 2))) {
+//                 if(this.position.y > hero.position.y) {this.move({y: -1})}
+//                 else if(this.position.y < hero.position.y) {this.move({y: +1})}
+//             } else {
+//                 if(this.position.x > hero.position.x) {this.move({x: -1})}
+//                 else if(this.position.x < hero.position.x) {this.move({x: +1})}
+//             }
+//         }
+//     }
+//     move(movement) {
+//         movement.x = movement.x || 0
+//         movement.y = movement.y || 0
+//
+//         game.forEach((entity) => {
+//             if(entity != this) {
+//                 if(this.position.x + movement.x == entity.position.x
+//                 && this.position.y + movement.y == entity.position.y) {
+//                     if(entity instanceof Hero) {
+//                         if(!!entity.isAttacked) {
+//                             entity.isAttacked()
+//                         }
+//                     }
+//                     movement.x = 0
+//                     movement.y = 0
+//                 }
+//             }
+//         })
+//
+//         this.position.x += movement.x
+//         this.position.y += movement.y
+//     }
+//     isAttacked() {
+//         this.health -= 1
+//         if(this.health <= 0) {
+//             if(Math.abs(this.position.x - altar.position.x) <= 1
+//             && Math.abs(this.position.y - altar.position.y) <= 1) {
+//                 hero.score += 2
+//                 altar.isActive = true
+//                 window.setTimeout(() => {
+//                     altar.isActive = false
+//                 }, 250)
+//             }
+//             game.splice(game.indexOf(this), 1)
+//             game.push(new Monster())
+//             hero.score += 1
+//         }
+//     }
+// }
