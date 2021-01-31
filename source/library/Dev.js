0e7e246b2f32c361cd2ec99a97392fb89de9eccd
Andrew McPherson <ehgoodenough@gmail.com>
2021-01-31 11:29:47 -0800

    Yet again disabled dev tools.

--- source/library/Dev.js
+++ source/library/Dev.js
@@ -3,7 +3,6 @@ import Stats from "stats.js"
 const dev = new class Dev {
     constructor() {
         this.isEnabled = false // !!
-        this.isEnabled = true
         if(this.isEnabled == false) return
 
         this.stats = new Stats()
